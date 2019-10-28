a03c68afc4263dd980a892b2d85aa3e486d23f0d
Gunnar Unger <gunnar.unger@gmail.com>
2019-10-29 09:17:01 +1100

    update

--- m/library/shared-data.js
+++ m/library/shared-data.js
@@ -18,11 +18,72 @@ m.load=function(){
         $('#export_section__ID').show();
     }
 }
+//-------------------------------
+/*
+m.export_records=function(){
+    tabledata=m.Table;
+    m.Table=$vm.module_list['participant-data'].Table;
+    var participant_rec={};
+    var req={cmd:"export",table:m.Table,I1:m.I1,search:$('#keyword__ID').val()}
+    open_model__ID();
+    $vm.request(req,function(N,i,txt){
+        //console.log(i+"/"+N);
+        $('#msg__ID').text((100*i/N).toFixed(0)+"%");
+        if(i==-1){
+            var len=txt.length;
+            n_txt="["+txt.substring(5,len-9)+"]";
+            participant_rec=JSON.parse(n_txt);
+            console.log(JSON.stringify(participant_rec))
+            //$vm.download_csv(m.Table+".csv",o);
+            close_model__ID();
+            m.Table=tabledata;
+            var req={cmd:"export",table:m.Table,I1:m.I1,search:$('#keyword__ID').val()}
+            open_model__ID();
+            $vm.request(req,function(N,i,txt){
+                //console.log("B"+i+"/"+N);
+                $('#msg__ID').text((100*i/N).toFixed(0)+"%");
+                if(i==-1){
+                    var len=txt.length;
+                    var data_rec="["+txt.substring(5,len-9)+"]";
+                    var o=JSON.parse(data_rec);
+                    var fields_ex=m.fields.replace("_Participant_ID","Participant_uid")
+                    var export_fields=fields_ex.split(',');
+                    //Order by m.fields
+                    export_fields=export_fields.slice(4,export_fields.length-3);
+                    var oo=JSON.parse(JSON.stringify(o,export_fields));
+                    //Create an empty item so download.csv will create all headings
+                    var item={}
+                    for(var i=0;i<export_fields.length;i++){
+                        item[export_fields[i]]="";
+                    }
+                    var output_data=[];
+                    for(var i=0;i<participant_rec.length;i++){
+                        for (var k=0;k<oo.length;k++){
+                            if(oo[k].Participant_uid==participant_rec[i].ID){
+                                output_data.push(oo[k]);
+                                break;
+                            }
+                            if(k==oo.length-1) {item.Participant_uid=participant_rec[i].ID; output_data.push(item)}
+                        }
+                    }
+                    var tmp=JSON.stringify(output_data).replace(/Participant_uid/g,"Participant ID")
+                    output_data=JSON.parse(tmp);
+                    //console.log(JSON.stringify(output_data))
+                    $vm.download_csv(m.Table+".csv",output_data);
+                    close_model__ID();
+                }
+            });
+        }
+    });
+    
+}
+*/
 //-------------------------------------
 m.cell_render=function(records,I,field,td){
     switch(field){
         case '_Status':
-            td.html("<span style='color:"+records[I].Data['sysStatus']+"'>&#x25cf;</span>");
+            td.html("<span style='color:"+records[I].Data['sysStatus']+"'><i class='fas fa-circle'></i></span>");
+            td.css('text-align','center');
             break;
         case '_Participant_ID':
             td.html(records[I].Data.Participant_uid);
@@ -33,6 +94,17 @@ m.cell_render=function(records,I,field,td){
             td.html("<u style='cursor:pointer'>Notes</u>");
             td.find('u').on('click',function(){
                 $vm.load_module(notes,'',{task_name:m.task_name,task_uid:records[I].UID,participant_uid:records[I].Data.Participant_uid})
+            })
+            break;
+        case '_Lock':
+            var lk=0; if(records[I].LK==1) lk=1;
+            var       h="<u i="+I+" style='cursor:pointer;color:green;'><i class='fa fa-lock-open'></i></u>";
+            if(lk==1) h="<u i="+I+" style='cursor:pointer;color:red;'><i class='fa fa-lock'></i></u>";
+            td.html(h);
+            td.css('text-align','center');
+            td.find('u').on('click',function(){
+                var _i=$(this).attr('i');
+                process_lock(_i);
             })
             break;
     }
@@ -111,4 +183,16 @@ m.new=function(){
         $vm.load_module(m.form_module,'',{participant_record:participant_record,goback:1});
     }
 }
-//-------------------------------------
+//-------------------------------------    
+var process_lock=function(I){
+    var lk=0; if(m.records[I].LK==1) lk=1;
+    var id=m.records[I]._id;    
+    var to_do_lock=0; if(lk==0) to_do_lock=1;
+    $vm.request({cmd:"lock",id:id,table:m.Table,lock:to_do_lock},function(res){
+        //console.log(res)
+        var $td=$('#grid__ID tr:nth-child('+(I+2)+')').find('td').eq(2);
+        m.records[I].LK=to_do_lock;
+        m.cell_render(m.records,I,'_Lock',$td);
+    });
+
+}//-------------------------------------
--- module-list.js
+++ module-list.js
@@ -1,34 +1,37 @@
 (function(){
     var modules={
-        "panel-main-ad036":    			{url:"$H/p/panel-main.html",router:1},
-        "panel-child-ad036":    			{url:"$H/p/panel-child.html"},
+        "panel-main":    			{url:"$H/m/panel-main.html",router:1},
+        "panel-child":    			{url:"$H/m/panel-child.html"},
         
         "participant-data":   		{url:"$H/m/participant/participant-data.html",Table:"ad036-participant",form_module:"participant-form",router:1,
-                                            child_panel:"panel-child-ad036",
+                                            child_panel:"panel-child",
                                             questionnaire_setup:"online-questionnaire-setup-ad036",
                                             online_questionnaire:"online-questionnaire-app-ad036",
                                             participant_id:{field1:"Subject_ID",field2:"Subject_Initials"}
                                         },
         "participant-form":   		{url:"$H/m/participant/participant-form.html",Table:"ad036-participant"},
 
-        "online-questionnaire-setup-ad036": 	{url:"$H/online-questionnaire/m/setup.html",Table:"ad036-participant"},
-        "online-questionnaire-app-ad036":    {url:"$H/online-questionnaire/index.html"},
+        "online-questionnaire-setup-ad036": 	{url:"$H/m/oq-setup.html",Table:"ad036-participant"},
+        "online-questionnaire-app-ad036":    {url:"$H/oq.html"},
 
-        "notes":  		  	  			{url:"$H/m/notes.html"},
-        "notes-data":  	    {url:"$H/m/notes-data.html",Table:"ad036-notes",form_module:"notes-form",router:1},
-        "notes-form":  	    {url:"$H/m/notes-form.html",Table:"ad036-notes"},
+        "notes-data":  	                    {url:"$H/m/library/notes-data.html",Table:"ad036-notes",form_module:"notes-form",router:1},
+        "notes-form":  	                    {url:"$H/m/library/notes-form.html",Table:"ad036-notes"},
 
 
         "ad036-w2-1back-data":      {url:"$H/m/neurocog/nback2-data.html",Table:"ad036-w2-1back",form_module:"ad036-w2-1back-form",task_name:"Week 2 - 1 Back"},
         "ad036-w2-1back-form":		{url:"$H/m/neurocog/nback2-form.html",Table:"ad036-w2-1back",task_name:"1 Back"},
         "ad036-w2-2back-data":      {url:"$H/m/neurocog/nback2-data.html",Table:"ad036-w2-2back",form_module:"ad036-w2-2back-form",task_name:"Week 2 - 2 Back"},
         "ad036-w2-2back-form":		{url:"$H/m/neurocog/nback2-form.html",Table:"ad036-w2-2back",task_name:"2 Back"},
+        "ad036-practice-2back-data":      {url:"$H/m/neurocog/nback2-data.html",Table:"ad036-practice-2back",form_module:"ad036-practice-2back-form",task_name:"Practice - 2 Back"},
+        "ad036-practice-2back-form":		{url:"$H/m/neurocog/nback2-practice-form.html",Table:"ad036-practice-2back",task_name:"2 Back"},
         "ad036-w2-3back-data":      {url:"$H/m/neurocog/nback2-data.html",Table:"ad036-w2-3back",form_module:"ad036-w2-3back-form",task_name:"Week 2 - 3 Back"},
         "ad036-w2-3back-form":		{url:"$H/m/neurocog/nback2-form.html",Table:"ad036-w2-3back",task_name:"3 Back"},
         "ad036-w2-dsst-data":		{url:"$H/m/neurocog/dsst-data.html",Table:"ad036-w2-dsst",form_module:"ad036-w2-dsst-form",task_name:"Week 2 - Digital Symbol Substitution Task"},
         "ad036-w2-dsst-form":		{url:"$H/m/neurocog/dsst-form.html",Table:"ad036-w2-dsst",task_name:"Digital Symbol Substitution Task"},
-        "ad036-w2-aused90-data":	{url:"$H/m/aused-90-data.html",Table:"ad036-w2-aused90",form_module:"ad036-w2-aused90-form",task_name:"Week 2 - Aused 90 mins"},
-        "ad036-w2-aused90-form":	{url:"$H/m/aused-90-form.html",Table:"ad036-w2-aused90"},
+        "ad036-practice-dsst-data":		{url:"$H/m/neurocog/dsst-data.html",Table:"ad036-practice-dsst",form_module:"ad036-practice-dsst-form",task_name:"Practice - Digital Symbol Substitution Task"},
+        "ad036-practice-dsst-form":		{url:"$H/m/neurocog/dsst-practice-form.html",Table:"ad036-practice-dsst",task_name:"Digital Symbol Substitution Task"},
+        "ad036-w2-aused90-data":	{url:"$H/m/library/aused-90-data.html",Table:"ad036-w2-aused90",form_module:"ad036-w2-aused90-form",task_name:"Week 2 - Aused 90 mins"},
+        "ad036-w2-aused90-form":	{url:"$H/m/library/aused-90-form.html",Table:"ad036-w2-aused90"},
 
         "ad036-w6-1back-data":      {url:"$H/m/neurocog/nback2-data.html",Table:"ad036-w6-1back",form_module:"ad036-w6-1back-form",task_name:"Week 2 - 1 Back"},
         "ad036-w6-1back-form":		{url:"$H/m/neurocog/nback2-form.html",Table:"ad036-w6-1back",task_name:"1 Back"},
@@ -38,8 +41,8 @@
         "ad036-w6-3back-form":		{url:"$H/m/neurocog/nback2-form.html",Table:"ad036-w6-3back",task_name:"3 Back"},
         "ad036-w6-dsst-data":		{url:"$H/m/neurocog/dsst-data.html",Table:"ad036-w6-dsst",form_module:"ad036-w6-dsst-form",task_name:"Week 2 - Digital Symbol Substitution Task"},
         "ad036-w6-dsst-form":		{url:"$H/m/neurocog/dsst-form.html",Table:"ad036-w6-dsst",task_name:"Digital Symbol Substitution Task"},
-        "ad036-w6-aused90-data":	{url:"$H/m/aused90-data.html",Table:"ad036-w6-aused90",form_module:"ad036-w6-aused90-form",task_name:"Week 2 - Aused 90 mins"},
-        "ad036-w6-aused90-form":	{url:"$H/m/aused90-form.html",Table:"ad036-w6-aused90"},
+        "ad036-w6-aused90-data":	{url:"$H/m/library/aused-90-data.html",Table:"ad036-w6-aused90",form_module:"ad036-w6-aused90-form",task_name:"Week 2 - Aused 90 mins"},
+        "ad036-w6-aused90-form":	{url:"$H/m/library/aused-90-form.html",Table:"ad036-w6-aused90"},
 
         "ad036-w2-e-ess-data":		{url:"$H/m/ess-data.html",Table:"ad036-w2-e-ess",form_module:"ad036-w2-e-ess-form",task_name:"Week 2 Evening - Epworth Sleepiness Scale (ESS)"},
         "ad036-w2-e-ess-form":		{url:"$H/m/ess-form.html",Table:"ad036-w2-e-ess",task_name:"Epworth Sleepiness Scale (ESS)"},
@@ -62,7 +65,11 @@
     }
     for(p in modules){
         $vm.module_list[p]=modules[p];
-        $vm.hosting_path=$vm.hosting_path.replace('online-questionnaire','')
         $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
+    }
+    if(window.location.toString().indexOf('tb=demo')!=-1){
+        for(p in $vm.module_list){
+            $vm.module_list[p].Table="demo-"+$vm.module_list[p].Table;
+        }
     }
 })();
