1b87f8d44cf3f9b0d4b0c241c2ac129a9fda59ab
Gunnar Unger <gunnar.unger@gmail.com>
2021-01-19 15:44:54 +1100

    update

--- m/neurocog/nback.js
+++ m/neurocog/nback.js
@@ -10,10 +10,7 @@ $('#D__ID').on('load',function(){
     $('#participant_div__ID').hide()
     $('#thirdparty_container__ID').css('padding-top','100px');
     $('#thirdparty_container__ID').css('height',$vm.min_height);
-    third_party("thirdparty_container__ID",data_process);
-    //data={}
-    //calc(data)
-    //console.log(data)
+    new_start();
 })
 //------------------------------------
 var ClearIntervals = function()
@@ -183,281 +180,300 @@ var data_process=function(data){
     });
     //--------------------------------------------------------
 }
-//------------------------------------
-var third_party=function(containerID,callback)
-{
+var new_start=function(){
     $('.start-button').hide();
     ClearIntervals();
-    $('#'+containerID).css('background-color','#000000');
-    var canvas = document.getElementById('main_canvas__ID');
-    var ctx = canvas.getContext('2d');
-    var scale = 4;
-    var angle = 60;
-    var radius1 = 10;
-    var radius2 = 35;
-    var numpretrials = nback;
-    var cuestarttostimulusstarttime = 1500;
-    var interstimulustime = 4500;
-    var cuevisibletime = 200;
-    var stimulusvisibletime = 250;
-    var practice = false;
-    var instructions = [
-                        "#instructions_1__ID",
-                        ];
-    var letters = ['B', 'C', 'F', 'H', 'L', 'M', 'P', 'T', 'W', 'X', 'Y', 'Z'];
-    var first = null;
-    var trials = [];
-    var responded = true;
-    var current = 0;
-    var startTime;
-    var width = canvas.width;
-    var height = canvas.height;
-    var midX = width / 2;
-    var midY = height / 2;
-    var separation = 140;
-    var finished = false;
-    function PointOnCircle(a, r)
-    {
-        var rad = a / 180 * Math.PI;
-        var ret = { x : -Math.cos(rad) * r * scale + midX, y : -Math.sin(rad) * r * scale + midY };
-        return ret;
-    }
-    var positions = [
-                        PointOnCircle(-angle, radius1),
-                        PointOnCircle(0, radius1),
-                        PointOnCircle(angle, radius1),
-                        PointOnCircle(180 - angle, radius1),
-                        PointOnCircle(180, radius1),
-                        PointOnCircle(180 + angle, radius1),
-                        PointOnCircle(-angle, radius2),
-                        PointOnCircle(0, radius2),
-                        PointOnCircle(angle, radius2),
-                        PointOnCircle(180 - angle, radius2),
-                        PointOnCircle(180, radius2),
-                        PointOnCircle(180 + angle, radius2),
-                    ];
-    var correct = 0;
-    var missed = 0;
-    var wrong = 0;
-    var results = {
+    $('#thirdparty_container__ID').css('background-color','#000000');
+    Instructions();
+    finished=false;
+    results = {
         Time : [],
         Number : [],
         KeyResponse : [],
     };
-    var responce = {};
-    instructions.forEach(function(i) { $(i).hide(); });
-    ctx.clearRect(0, 0, width, height);
-    ctx.fillStyle = 'white';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.font = '32px arial';
-    function Instructions()
-    {
-        $('#main_canvas__ID').hide();
-        $('.start-button').show();
-        $(instructions[current]).show();
-    }
-    function RandomExcept(maxExcl, exception)
-    {
-        var ret = 0;
-        do
-        {
-            ret = Math.floor(Math.random() * maxExcl);
-        } while (ret == exception);
-        return ret;
-    }
-    function ReferenceTrial(cur)
-    {
-        if (nback == 1)
-            return Math.max(0, cur - 1);
-        else if (nback == 2)
-            return Math.max(0, cur - 2);
-        else if (nback == 3)
-            return Math.max(0, cur - 3);
-    }
-    function Start()
-    {
-        var start_time= new Date();
-        var hh = start_time.getHours()
-        var mn = start_time.getMinutes()
-        var ss = start_time.getSeconds()
-        if(mn<10){mn='0'+mn;}
-        if(hh<10){hh='0'+hh;}
-        if(ss<10){ss='0'+ss;}
-        responce.Start_Time=hh+':'+mn+':'+ss;
-        $('.start-button').hide();
-        $('#participant_div__ID').css("visibility", "hidden");
-        $(instructions[current]).hide();
-        $('#main_canvas__ID').show();
-        marks = [];
-        trials=[];
-        Clear();
-        for (var i=0; i<numpretrials; i++)
-        {
-            var t = {
-                        l: Math.floor(Math.random() * letters.length),
-                        p: Math.floor(Math.random() * positions.length),
-                        pre: true,
-                    }
-            trials.push(t);
-        }
-
-        var p1=0,p2=0,pv;
-        for (var i=0; i<numtrials; i++)
-        {
-            var ref = ReferenceTrial(trials.length);
-            if(p1==parseInt((numtrials-3)/2) || p2==parseInt((numtrials-3)/2) ) {
-                if(p2==parseInt((numtrials-3)/2)) {pv=trials[ref].p;}
-                else pv=RandomExcept(positions.length, trials[ref].p);
-            }
-            else { if(Math.random() < 0.5 ){
-                        pv=trials[ref].p;
-                        if(i>2) p1++;
-                    }
-                    else{
-                        pv=RandomExcept(positions.length, trials[ref].p);
-                        if(i>2) p2++;
-                    }
-            }
-            var t = {
-                        id: i+1,
-                        l: Math.floor(Math.random() * letters.length),
-                        p: pv,
-                        pre: false,
-                    }
-            trials.push(t);
-        }
-        
-        for (var i=0; i<trials.length; i++)
-        {
-            var t = trials[i];
-            console.log(i + ' ' + t.l + ' ' + t.p) ;
-        }
-        current = -1;
-        Next();
-        intervals.push(setInterval(Next, interstimulustime));
-    }
-    function Next()
-    {
-        if (!responded)
-        {
-            SaveResults('O');
-        }
-        current++;
-
-        if (current >= trials.length)
-        {
-            ClearIntervals();
-            Finish();
-        }
-        else
-        {
-            Clear();
-            Cue();
-            setTimeout(Clear, cuevisibletime);
-            setTimeout(Stimulus, cuestarttostimulusstarttime);
-            setTimeout(Clear, cuestarttostimulusstarttime+stimulusvisibletime);
-        }
-    }
-
-    function Stimulus()
-    {
-        ctx.fillStyle = 'white';
-        var t = trials[current];
-        var l = letters[t.l];
-        var pt = positions[t.p];
-        ctx.fillText(l, pt.x, pt.y);
-        responded = false;
-        startTime = Date.now();
-        /*for (var i=0; i<positions.length; i++)
-        {
-            var l = letters[Math.floor(Math.random() * letters.length)];
-            var pt = positions[i];
-            ctx.fillText(l, pt.x, pt.y);
-        }*/
-    }
-    function Cue()
-    {
-        ctx.beginPath();
-        ctx.fillStyle = '#00ff00';
-        ctx.arc(midX, midY, 20, 0, 2 * Math.PI);
-        ctx.fill();
-    }
-    function Clear()
-    {
-        ctx.clearRect(0, 0, width, height);
-        ctx.beginPath();
-        ctx.fillStyle = 'red';
-        ctx.arc(midX, midY, 5, 0, 2 * Math.PI);
-        ctx.fill();
-        //ctx.fillText(correct + ' ' + missed + ' ' + wrong, 100, 550);
-    }
-    function SaveResults(resp)
-    {
-        responded = true;
-        var endTime = Date.now();
-        var t = trials[current];
-        if (t.pre)
-        {
-            results.Number.push('PRE');
-            results.Time.push(0);
-            results.KeyResponse.push('-');
-        }
-        else
-        {
-            results.Number.push(t.id);
-            results.Time.push(endTime - startTime);
-            results.KeyResponse.push(resp);
-        }
-        console.log(current + ' ' + resp) ;
-    }
-    var Finish = function()
-    {
-        if ($("#main_canvas__ID").is(':hidden')) //hacky and ugly way :(
-            return;
-        if (!finished)
-        {
-            finished = true;
-            
-            responce.NNO = results.Number.join(",");
-            responce.NRT = results.Time.join(",");
-            responce.NAC = results.KeyResponse.join(",");
-            callback(responce);
-        }
-    }
-    $(document).keypress(function(e){
-        if ($("#main_canvas__ID").is(':hidden')) //hacky and ugly way :(
-            return;
-        if (!responded)
-        {
-            var t = trials[current];
-            var rt = trials[ReferenceTrial(current)];
-            var key = e.key.toLowerCase();
-            console.log(key)
-            if (key == 'm')
-            {
-                if (rt.p == t.p)
-                    SaveResults('CM');
-                else
-                    SaveResults('FN');
-            }
-            if (key == 'n')
-            {
-                if (rt.p == t.p)
-                    SaveResults('FM');
-                else
-                    SaveResults('CN');
-            }
-            if (key != 'n' && key != 'm') SaveResults('W');
-        }
-        e.preventDefault();
-    });
-    Instructions();
-    $('.start-button').on('click',function(){
-        Start();
-    });
-    //------------------------------------
+    responce = {};
 }
+//------------------------------------
+var canvas = document.getElementById('main_canvas__ID');
+var ctx = canvas.getContext('2d');
+var scale = 4;
+var angle = 60;
+var radius1 = 10;
+var radius2 = 35;
+var numpretrials = nback;
+var cuestarttostimulusstarttime = 1500;
+var interstimulustime = 4500;
+var cuevisibletime = 200;
+var stimulusvisibletime = 250;
+var practice = false;
+var instructions = [
+                    "#instructions_1__ID",
+                    ];
+var letters = ['B', 'C', 'F', 'H', 'L', 'M', 'P', 'T', 'W', 'X', 'Y', 'Z'];
+var first = null;
+var trials = [];
+var responded = true;
+var current = 0;
+var startTime;
+var width = canvas.width;
+var height = canvas.height;
+var midX = width / 2;
+var midY = height / 2;
+var separation = 140;
+var finished = false;
+//-------------------------------------
+function PointOnCircle(a, r)
+{
+    var rad = a / 180 * Math.PI;
+    var ret = { x : -Math.cos(rad) * r * scale + midX, y : -Math.sin(rad) * r * scale + midY };
+    return ret;
+}
+var positions = [
+                    PointOnCircle(-angle, radius1),
+                    PointOnCircle(0, radius1),
+                    PointOnCircle(angle, radius1),
+                    PointOnCircle(180 - angle, radius1),
+                    PointOnCircle(180, radius1),
+                    PointOnCircle(180 + angle, radius1),
+                    PointOnCircle(-angle, radius2),
+                    PointOnCircle(0, radius2),
+                    PointOnCircle(angle, radius2),
+                    PointOnCircle(180 - angle, radius2),
+                    PointOnCircle(180, radius2),
+                    PointOnCircle(180 + angle, radius2),
+                ];
+var correct = 0;
+var missed = 0;
+var wrong = 0;
+var results = {
+    Time : [],
+    Number : [],
+    KeyResponse : [],
+};
+var responce = {};
+instructions.forEach(function(i) { $(i).hide(); });
+ctx.clearRect(0, 0, width, height);
+ctx.fillStyle = 'white';
+ctx.textAlign = 'center';
+ctx.textBaseline = 'middle';
+ctx.font = '32px arial';
+//-------------------------------------
+function Instructions()
+{
+    $('#main_canvas__ID').hide();
+    $('.start-button').show();
+    $("#instructions_1__ID").show();
+    console.log("Instructions")
+}
+//-------------------------------------
+function RandomExcept(maxExcl, exception)
+{
+    var ret = 0;
+    do
+    {
+        ret = Math.floor(Math.random() * maxExcl);
+    } while (ret == exception);
+    return ret;
+}
+//-------------------------------------
+function ReferenceTrial(cur)
+{
+    if (nback == 1)
+        return Math.max(0, cur - 1);
+    else if (nback == 2)
+        return Math.max(0, cur - 2);
+    else if (nback == 3)
+        return Math.max(0, cur - 3);
+}
+//-------------------------------------
+function Start()
+{
+    var start_time= new Date();
+    var hh = start_time.getHours()
+    var mn = start_time.getMinutes()
+    var ss = start_time.getSeconds()
+    if(mn<10){mn='0'+mn;}
+    if(hh<10){hh='0'+hh;}
+    if(ss<10){ss='0'+ss;}
+    responce.Start_Time=hh+':'+mn+':'+ss;
+    $('.start-button').hide();
+    $('#participant_div__ID').css("visibility", "hidden");
+    $("#instructions_1__ID").hide();
+    $('#main_canvas__ID').show();
+    marks = [];
+    trials=[];
+    Clear();
+    for (var i=0; i<numpretrials; i++)
+    {
+        var t = {
+                    l: Math.floor(Math.random() * letters.length),
+                    p: Math.floor(Math.random() * positions.length),
+                    pre: true,
+                }
+        trials.push(t);
+    }
+
+    var p1=0,p2=0,pv;
+    for (var i=0; i<numtrials; i++)
+    {
+        var ref = ReferenceTrial(trials.length);
+        if(p1==parseInt((numtrials-3)/2) || p2==parseInt((numtrials-3)/2) ) {
+            if(p2==parseInt((numtrials-3)/2)) {pv=trials[ref].p;}
+            else pv=RandomExcept(positions.length, trials[ref].p);
+        }
+        else { if(Math.random() < 0.5 ){
+                    pv=trials[ref].p;
+                    if(i>2) p1++;
+                }
+                else{
+                    pv=RandomExcept(positions.length, trials[ref].p);
+                    if(i>2) p2++;
+                }
+        }
+        var t = {
+                    id: i+1,
+                    l: Math.floor(Math.random() * letters.length),
+                    p: pv,
+                    pre: false,
+                }
+        trials.push(t);
+    }
+    
+    for (var i=0; i<trials.length; i++)
+    {
+        var t = trials[i];
+        console.log(i + ' ' + t.l + ' ' + t.p) ;
+    }
+    current = -1;
+    Next();
+    intervals.push(setInterval(Next, interstimulustime));
+}
+//-------------------------------------
+function Next()
+{
+    if (!responded)
+    {
+        SaveResults('O');
+    }
+    current++;
+    if (current >= trials.length)
+    {
+        ClearIntervals();
+        Finish();
+    }
+    else
+    {
+        Clear();
+        Cue();
+        setTimeout(Clear, cuevisibletime);
+        setTimeout(Stimulus, cuestarttostimulusstarttime);
+        setTimeout(Clear, cuestarttostimulusstarttime+stimulusvisibletime);
+    }
+}
+//-------------------------------------
+function Stimulus()
+{
+    ctx.fillStyle = 'white';
+    var t = trials[current];
+    var l = letters[t.l];
+    var pt = positions[t.p];
+    ctx.fillText(l, pt.x, pt.y);
+    responded = false;
+    startTime = Date.now();
+    /*for (var i=0; i<positions.length; i++)
+    {
+        var l = letters[Math.floor(Math.random() * letters.length)];
+        var pt = positions[i];
+        ctx.fillText(l, pt.x, pt.y);
+    }*/
+}
+//-------------------------------------
+function Cue()
+{
+    ctx.beginPath();
+    ctx.fillStyle = '#00ff00';
+    ctx.arc(midX, midY, 20, 0, 2 * Math.PI);
+    ctx.fill();
+}
+//-------------------------------------
+function Clear()
+{
+    ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.fillStyle = 'red';
+    ctx.arc(midX, midY, 5, 0, 2 * Math.PI);
+    ctx.fill();
+    //ctx.fillText(correct + ' ' + missed + ' ' + wrong, 100, 550);
+}
+//-------------------------------------
+function SaveResults(resp)
+{
+    responded = true;
+    var endTime = Date.now();
+    var t = trials[current];
+    if (t.pre)
+    {
+        results.Number.push('PRE');
+        results.Time.push(0);
+        results.KeyResponse.push('-');
+    }
+    else
+    {
+        results.Number.push(t.id);
+        results.Time.push(endTime - startTime);
+        results.KeyResponse.push(resp);
+    }
+    console.log(current + ' ' + resp) ;
+}
+//-------------------------------------
+var Finish = function()
+{
+
+    if ($("#main_canvas__ID").is(':hidden')) //hacky and ugly way :(
+            return;
+    if (!finished)
+    {
+        finished = true;
+        
+        responce.NNO = results.Number.join(",");
+        responce.NRT = results.Time.join(",");
+        responce.NAC = results.KeyResponse.join(",");
+        //console.log("finish: "+JSON.stringify(responce))
+        data_process(responce);
+    }
+}
+//-------------------------------------
+$(document).keypress(function(e){
+    if ($("#main_canvas__ID").is(':hidden')) //hacky and ugly way :(
+        return;
+    if (!responded)
+    {
+        var t = trials[current];
+        var rt = trials[ReferenceTrial(current)];
+        var key = e.key.toLowerCase();
+        console.log(key)
+        if (key == 'm')
+        {
+            if (rt.p == t.p)
+                SaveResults('CM');
+            else
+                SaveResults('FN');
+        }
+        if (key == 'n')
+        {
+            if (rt.p == t.p)
+                SaveResults('FM');
+            else
+                SaveResults('CN');
+        }
+        if (key != 'n' && key != 'm') SaveResults('W');
+    }
+    e.preventDefault();
+});
+//------------------------------------
+$('.start-button').on('click',function(){
+    Start();
+});
 //-------------------------------------
 function standardDeviation(values){
     var avg = average(values); 
